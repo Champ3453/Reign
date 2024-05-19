c23aa53ac56574937b58351b3ccecc435cc19678
Your Buddy <86676094+Champ3453@users.noreply.github.com>
2024-05-19 12:00:35 +0200

    Add files via upload

--- Reign-Experimental.lua
+++ Reign-Experimental.lua
@@ -1,81 +1,96 @@
 local Reign = {};
 
--- Functions
 function Reign:Make(Data)
-    local instance = Instance.new(Data.Instance);
+	local instance = Instance.new(Data.Instance);
 
-    if Data.Properties then
-        for prop, value in Data.Properties do
-            instance[prop] = value;
-        end
-    end
+	if Data.Properties then
+		for prop, value in Data.Properties do
+			instance[prop] = value;
+		end
+	end
 
-    if Data.Attributes then
-        for attr, value in Data.Attributes do
-            instance:SetAttribute(attr, value);
-        end
-    end
+	if Data.Attributes then
+		for attr, value in Data.Attributes do
+			instance:SetAttribute(attr, value);
+		end
+	end
 
-    if Data.Tags then
-        for value in Data.Tags do
-            instance:AddTag(value);
-        end
-    end
+	if Data.Tags then
+		for value in Data.Tags do
+			instance:AddTag(value);
+		end
+	end
 
-    if instance:IsA("TextButton") or instance:IsA("ImageButton") then
-        if Data.OnClick then
-            instance[Data.OnClick.Button]:Connect(Data.OnClick.Callback);
-        end
-    end
+	if instance:IsA("TextButton") or instance:IsA("ImageButton") then
+		if Data.OnClick then
+			instance[Data.OnClick.Button]:Connect(Data.OnClick.Callback);
+		end
+	end
 
-    return instance;
+	return instance;
 end
 
 function Reign:Update(Data)
-    local instance = Data.Instance;
+	local instance = Data.Instance;
 
-    if Data.Properties then
-        for prop, value in Data.Properties do
-            instance[prop] = value;
-        end
-    end
+	if Data.Properties then
+		for prop, value in Data.Properties do
+			instance[prop] = value;
+		end
+	end
 
-    if Data.Attributes then
-        for attr, value in Data.Attributes do
-            instance:SetAttribute(attr, value);
-        end
-    end
+	if Data.Attributes then
+		for attr, value in Data.Attributes do
+			instance:SetAttribute(attr, value);
+		end
+	end
 
-    if Data.Tags then
-        for tag, value in Data.Tags do
-            instance:RemoveTag(tag);
-            instance:AddTag(value);
-        end
-    end
+	if Data.Tags then
+		for tag, value in Data.Tags do
+			instance:RemoveTag(tag);
+			instance:AddTag(value);
+		end
+	end
 
-    if instance:IsA("TextButton") or instance:IsA("ImageButton") then
-        if Data.OnClick then
-            instance[Data.OnClick.Button]:Connect(Data.OnClick.Callback);
-        end
-    end
+	if instance:IsA("TextButton") or instance:IsA("ImageButton") then
+		if Data.OnClick then
+			instance[Data.OnClick.Button]:Connect(Data.OnClick.Callback);
+		end
+	end
 
-    return instance;
+	return instance;
 end
 
 function Reign:OnKeydown(Data)
-    local key = Data.Key;
-    local callback = Data.Callback;
+	local state = false;
 
-    game:GetService("UserInputService").InputChanged:Connect(function(input, gameProcessedEvent)
-        if gameProcessedEvent then return end;
-        if input.KeyCode == Enum.KeyCode[key] then
-            if input.UserInputState == Enum.UserInputState.Begin then
-                task.spawn(callback);
-            else
-                task.cancel(callback);
-            end
-        end
-    end)
+	local key = Data.Key;
+	local callback = Data.Callback;
+	local thread;
+
+	game:GetService("UserInputService").InputBegan:Connect(function(input, gameProcessedEvent)
+		if gameProcessedEvent then return end;
+		if input.KeyCode == Enum.KeyCode[key] then
+			thread = coroutine.create(function()
+				state = true;
+				callback(state);
+			end)
+			coroutine.resume(thread);
+			task.cancel(thread);
+		end
+	end)
+	
+	game:GetService("UserInputService").InputEnded:Connect(function(input, gameProcessedEvent)
+		if gameProcessedEvent then return end;
+		if input.KeyCode == Enum.KeyCode[key] then
+			thread = coroutine.create(function()
+				state = false;
+				callback(state);
+			end)
+			coroutine.resume(thread);
+			task.cancel(thread);
+		end
+	end)
 end
 
 return Reign;
