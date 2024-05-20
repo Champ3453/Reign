58d312bbd8ca5252582d83a53379123b29689040
Your Buddy <86676094+Champ3453@users.noreply.github.com>
2024-05-20 11:04:30 +0200

    Update
    [+] Added :OnKeypress()
    [+] Added :Transition()

--- ReignExperimental.lua
+++ ReignExperimental.lua
@@ -26,14 +26,24 @@ function Reign:Make(Data)
 			instance[Data.OnClick.Button]:Connect(Data.OnClick.Callback);
 		end
 	end
-	
+
 	if Data.OnKeydown then
 		local key = Data.OnKeydown.Key;
 		local callback = Data.OnKeydown.Callback;
 
 		Reign:OnKeydown({
 			Key = key;
-			Callback = callback
+			Callback = callback;
+		})
+	end
+
+	if Data.OnKeypress then
+		local key = Data.OnKeypress.Key;
+		local callback = Data.OnKeypress.Callback;
+
+		Reign:OnKeypress({
+			Key = key;
+			Callback = callback;
 		})
 	end
 
@@ -68,13 +78,20 @@ function Reign:Update(Data)
 		end
 	end
 	
-	if Data.OnKeydown then
-		local key = Data.OnKeydown.Key;
-		local callback = Data.OnKeydown.Callback;
+	if Data.Transition then
+		local time = Data.Transition.Time;
+		local es = Data.Transition.EasingStyle or Data.Transition.ES;
+		local ed = Data.Transition.EasingDirection or Data.Transition.ED;
+		local properties = Data.Transition.Properties or Data.Transition.Prop;
+		local play = Data.Transition.Play or false;
 
-		Reign:OnKeydown({
-			Key = key;
-			Callback = callback
+		Reign:Transition({
+			Instance = instance;
+			Time = time;
+			EasingStyle = es;
+			EasingDirection = ed;
+			Properties = properties;
+			Play = play;
 		})
 	end
 
@@ -84,7 +101,7 @@ end
 function Reign:OnKeydown(Data)
 	local key = Data.Key;
 	local callback = Data.Callback;
-	
+
 	local state;
 	local thread;
 
@@ -106,6 +123,35 @@ function Reign:OnKeydown(Data)
 			task.cancel(thread);
 		end
 	end)
+end
+
+function Reign:OnKeypress(Data)
+	local key = Data.Key;
+	local callback = Data.Callback;
+	local state = false;
+
+	game:GetService("UserInputService").InputBegan:Connect(function(input, gameProcessedEvent)
+		if gameProcessedEvent then return end;
+		if input.KeyCode == Enum.KeyCode[key] then
+			state = not state;
+			callback(state);
+		end
+	end)
+end
+
+function Reign:Transition(Data)
+	local instance = Data.Instance;
+	local time = Data.Time;
+	local es = Data.EasingStyle or Data.ES;
+	local ed = Data.EasingDirection or Data.ED;
+	local properties = Data.Properties or Data.Prop;
+	local play = Data.Play or false;
+
+	if play then
+		return game:GetService("TweenService"):Create(instance, TweenInfo.new(time, Enum.EasingStyle[es], Enum.EasingDirection[ed]), properties):Play();
+	else
+		return game:GetService("TweenService"):Create(instance, TweenInfo.new(time, Enum.EasingStyle[es], Enum.EasingDirection[ed]), properties);
+	end
 end
 
 return Reign;
